723c6558aa25c20bbd56de7568545a074db34efa
carloss-ziegler <chziegler445@gmail.com>
2022-10-03 16:16:12 -0300

    First Commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => (
+  <div className="w-full flex justify-center items-center">
+    <h1 className="font-bold text-white text-2xl mt-2">
+      Algo deu errado. Tente novamente!
+    </h1>
+  </div>
+);
+
+export default Error;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "e0d272a134msh85daf1c1f141d1cp13c731jsnc42136979148"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
